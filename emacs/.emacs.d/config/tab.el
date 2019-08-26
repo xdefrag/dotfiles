8071585114336b5320abe3788e66b89b6d756fee
Stanislaw Karkavin <me@xdefrag.dev>
2019-08-26 10:35:51 +0300

    Update emacs config

--- emacs/.emacs.d/config/tab.el
+++ emacs/.emacs.d/config/tab.el
@@ -16,6 +16,7 @@
 ;; Hooks to Disable Tabs
 (add-hook 'lisp-mode-hook 'disable-tabs)
 (add-hook 'emacs-lisp-mode-hook 'disable-tabs)
+(add-hook 'fsharp-mode-hook 'disable-tabs)
 
 ;; Language-Specific Tweaks
 (setq-default python-indent-offset custom-tab-width) ;; Python
