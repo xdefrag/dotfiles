175a5dce1b1ff23e5e01b9d7ca2e1a4d56295bf0
Stanislav Karkavin <skarkavin@ozon.ru>
2022-06-15 16:23:48 +0300

    Update private_dot_config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -150,7 +150,6 @@ require'packer'.startup(function(use)
       vim.api.nvim_set_keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], opts)
       vim.api.nvim_set_keymap('n', '<leader>sf', [[<cmd>lua require('telescope.builtin').fd({previewer = false})<CR>]], opts)
       vim.api.nvim_set_keymap('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], opts)
-      vim.api.nvim_set_keymap('n', '<leader>sq', [[<cmd>lua require('telescope.builtin').guickfix()<CR>]], opts)
     end
   }
 
@@ -212,13 +211,47 @@ require'packer'.startup(function(use)
   use {
     'neovim/nvim-lspconfig',
     config = function()
-      -- lspconfig setup
+      -- Mappings.
+      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+      local opts = { noremap=true, silent=true }
+      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+      -- Use an on_attach function to only map the following keys
+      -- after the language server attaches to the current buffer
+      local on_attach = function(client, bufnr)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        -- Mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local bufopts = { noremap=true, silent=true, buffer=bufnr }
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+        vim.keymap.set('n', '<space>wl', function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, bufopts)
+        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+        vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+        vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+        vim.keymap.set('n', '<space>lr', vim.lsp.codelens.run, bufopts)
+      end
+
       local lspconfig = require'lspconfig'
       local capabilities = vim.lsp.protocol.make_client_capabilities()
 
       lspconfig.gopls.setup{
-        -- on_attach = on_attach,
         capabilities = capabilities,
+        on_attach = on_attach,
         settings = {
           gopls = {
             env = {
@@ -232,8 +265,8 @@ require'packer'.startup(function(use)
       }
 
       lspconfig.sumneko_lua.setup {
-        -- on_attach = on_attach,
         capabilities = capabilities,
+        on_attach = on_attach,
         settings = {
           Lua = {
             runtime = {
