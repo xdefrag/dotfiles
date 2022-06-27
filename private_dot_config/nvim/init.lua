ef3de6bafec1b4f8c55c35da8af795920c48b7e9
Stanislav Karkavin <skarkavin@ozon.ru>
2022-06-27 15:13:58 +0300

    Update private_dot_config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -284,6 +284,7 @@ require 'packer'.startup(function(use)
             -- memoryMode = "DegradeClosed",
             gofumpt = true,
             staticcheck = true,
+            buildFlags = {'-tags=integration,statemachine_test'},
           },
         },
       }
@@ -545,7 +546,6 @@ require 'packer'.startup(function(use)
         lsp_document_formatting = false,
         dap_debug_keymap = true,
         run_in_floaterm = true,
-        build_tags = 'integration,statemachine_test',
       }
 
       vim.cmd('autocmd FileType go noremap <Leader>tn :GoTest -n<CR>')
@@ -556,7 +556,7 @@ require 'packer'.startup(function(use)
       vim.cmd('autocmd FileType go noremap <Leader>a :GoAlt<CR>')
       vim.cmd('autocmd FileType go noremap <Leader>f :GoImport<CR>')
 
-      -- vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
+      vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
     end
   }
 end)
