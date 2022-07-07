f01ded29cebbd7c514331a1afbdfe60f000560aa
Stanislav Karkavin <skarkavin@ozon.ru>
2022-07-07 17:13:47 +0300

    Update private_dot_config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -194,7 +194,7 @@ require 'packer'.startup(function(use)
       require 'telescope'.setup {
         extensions = {
           project = {
-            base_dirs = {},
+            base_dirs = { '~/Code', '~/ozon' },
             hidden_files = true,
             theme = "dropdown"
           }
