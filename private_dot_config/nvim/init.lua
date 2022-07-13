3c09f5425904f8918dcbd6b1da7ce241fbd9659a
Stanislav Karkavin <xdefrag@posteo.net>
2022-07-13 18:23:51 +0300

    Update private_dot_config/nvim/init.lua

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -332,6 +332,10 @@ require 'packer'.startup(function(use)
 
   use {
     'hrsh7th/nvim-cmp',
+    requires = {
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-emoji',
+    },
     config = function()
       local luasnip = require 'luasnip'
       local cmp = require 'cmp'
@@ -375,8 +379,36 @@ require 'packer'.startup(function(use)
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'neorg' },
+          { name = 'emoji' },
         },
       }
+
+      -- Set configuration for specific filetype.
+      cmp.setup.filetype('gitcommit', {
+        sources = cmp.config.sources({
+          { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+        }, {
+          { name = 'buffer' },
+        })
+      })
+
+      -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
     end
   }
 
